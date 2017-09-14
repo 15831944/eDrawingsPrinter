9e71ae7ff8624c8f95785098c1052197bbb6d7da
KC Juntunen <juntunenkc@lan.amstore.com>
2017-09-14 06:06:30 -0400

    Initial commit.

---
+++ eDrawingsPrinter/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace eDrawingsPrinter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.button1 = new System.Windows.Forms.Button();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.button2 = new System.Windows.Forms.Button();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.button3 = new System.Windows.Forms.Button();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button1.Location = new System.Drawing.Point(12, 467);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(109, 24);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Select Documents";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button2.Location = new System.Drawing.Point(127, 468);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "Print All";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // panel1
+      // 
+      this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel1.Location = new System.Drawing.Point(3, 3);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(557, 330);
+      this.panel1.TabIndex = 3;
+      // 
+      // listBox1
+      // 
+      this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Location = new System.Drawing.Point(3, 339);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(557, 106);
+      this.listBox1.TabIndex = 0;
+      // 
+      // button3
+      // 
+      this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button3.Location = new System.Drawing.Point(501, 468);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(75, 23);
+      this.button3.TabIndex = 5;
+      this.button3.Text = "Close";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.tableLayoutPanel1.ColumnCount = 1;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 2;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(563, 448);
+      this.tableLayoutPanel1.TabIndex = 6;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(588, 503);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Name = "Form1";
+      this.Text = "eDrawings Printer";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ eDrawingsPrinter/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace eDrawingsPrinter {
+  public partial class Form1 : Form {
+    private eDrawingHostControl.eDrawingControl eDrawingControl1;
+    private bool initialated = false;
+    private Stack listbox1items = new Stack();
+
+    public Form1() {
+      InitializeComponent();
+      Text = Text + " " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+    }
+
+    private void InitializeEDrawingComponent() {
+      this.eDrawingControl1 = new eDrawingHostControl.eDrawingControl();
+      // 
+      // eDrawingControl1
+      // 
+      this.panel1.Controls.Add(this.eDrawingControl1);
+      this.eDrawingControl1.BackColor = System.Drawing.Color.White;
+      this.eDrawingControl1.EnableFeatures = 32;
+      this.eDrawingControl1.Location = new System.Drawing.Point(1, 1);
+      this.eDrawingControl1.Name = "eDrawingControl1";
+      this.eDrawingControl1.Size = new System.Drawing.Size(this.panel1.Width - 1, this.panel1.Height - 1);
+      this.eDrawingControl1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+      this.eDrawingControl1.TabIndex = 0;
+      initialated = true;
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      string myFile = @"D:\shared\models\2010\power supply assembly.sldasm";
+
+      if (File.Exists(myFile))
+        eDrawingControl1.eDrawingControlWrapper.OpenDoc(myFile, false, false, true, "");
+      else {
+        this.openFileDialog1.InitialDirectory = Properties.Settings.Default.LastDir;
+        this.openFileDialog1.FileName = string.Empty;
+        this.openFileDialog1.Multiselect = true;
+        this.openFileDialog1.Filter = Properties.Settings.Default.OpenDialogFilter;
+        if (this.openFileDialog1.ShowDialog(this) == DialogResult.OK) {
+          foreach (string file in this.openFileDialog1.FileNames) {
+            Properties.Settings.Default.LastDir = Path.GetDirectoryName(this.openFileDialog1.FileNames[0]);
+            if (!listBox1.Items.Contains(file)) {
+              listBox1.Items.Add(file);
+            }
+          }
+        }
+      }
+    }
+
+    private void printlist() {
+      foreach (string item in listBox1.Items) {
+        listbox1items.Push(item);
+      }
+
+      print(new FileInfo((string)listbox1items.Pop()));
+    }
+
+    private void print(FileInfo file) {
+      eDrawingControl1.eDrawingControlWrapper.OpenDoc(file.FullName, false, false, true, "");
+    }
+
+    private void Form1_Load(object sender, EventArgs e) {
+      Location = Properties.Settings.Default.LastLocation;
+      Size = Properties.Settings.Default.LastSize;
+      if (initialated) {
+        ConnectEvents();
+      } else {
+        InitializeEDrawingComponent();
+        ConnectEvents();
+      }
+    }
+
+    private void DestructReconstruct() {
+      eDrawingControl1.eDrawingControlWrapper.CloseActiveDoc("");
+      foreach (Control c in Controls) {
+        if (c is eDrawingHostControl.eDrawingControl) {
+          Controls.Remove(c);
+        }
+      }
+      InitializeEDrawingComponent();
+    }
+
+    private void ConnectEvents() {
+      eDrawingControl1.eDrawingControlWrapper.OnFinishedLoadingDocument += eDrawingControlWrapper_OnFinishedLoadingDocument;
+      eDrawingControl1.eDrawingControlWrapper.OnFailedLoadingDocument += eDrawingControlWrapper_OnFailedLoadingDocument;
+      eDrawingControl1.eDrawingControlWrapper.OnFinishedPrintingDocument += eDrawingControlWrapper_OnFinishedPrintingDocument;
+    }
+
+    void eDrawingControlWrapper_OnFinishedPrintingDocument(string PrintJobName) {
+      print(new FileInfo((string)listbox1items.Pop()));
+    }
+
+    void eDrawingControlWrapper_OnFailedLoadingDocument(string FileName, int ErrorCode, string ErrorString) {
+      MessageBox.Show(string.Format("Failed to open '{0}' ([{1:x}] {2})", FileName, ErrorCode, ErrorString));
+    }
+
+    void eDrawingControlWrapper_OnFinishedLoadingDocument(string FileName) {
+      eDrawingControl1.eDrawingControlWrapper.Refresh();
+
+      PrintWait(FileName);
+    }
+
+    private async void PrintWait(string FileName) {
+      Task<bool> ip = innerprint(FileName);
+      await ip;
+    }
+
+    private async Task<bool> innerprint(string FileName) {
+      FileInfo f = new FileInfo(FileName);
+      int pages = eDrawingControl1.eDrawingControlWrapper.SheetCount;
+      eDrawingControl1.eDrawingControlWrapper.SetPageSetupOptions(
+        EModelView.EMVPrintOrientation.eLandscape, 1, 0, 0, 1, 7, GetDefaultPrinter(), 1, 1, 1, 1);
+      eDrawingControl1.eDrawingControlWrapper.Print5(false,
+        f.Name, false, false, false, EModelView.EMVPrintType.eScaleToFit, 4, 0, 0, false, 1, pages, "");
+      return false;
+    }
+
+    private void button2_Click(object sender, EventArgs e) {
+      printlist();
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+      Properties.Settings.Default.LastSize = Size;
+      Properties.Settings.Default.LastLocation = Location;
+      Properties.Settings.Default.Save();
+    }
+
+    private void button3_Click(object sender, EventArgs e) {
+      this.Close();
+    }
+
+    static string GetDefaultPrinter() {
+      PrinterSettings settings = new PrinterSettings();
+      foreach (string printer in PrinterSettings.InstalledPrinters) {
+        settings.PrinterName = printer;
+        if (settings.IsDefaultPrinter)
+          return printer;
+      }
+      return string.Empty;
+    }
+  }
+}
